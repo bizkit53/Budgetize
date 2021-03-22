5625c8dac76da9ae8a72d4728896e58ab210416f
bizkit53 <ciuba.piotr@gmail.com>
2021-03-22 10:47:55 +0100

    Home screen and main navigation reorganized

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,10 +1,9 @@
-import 'package:budgetize/bottomNavigationBar.dart';
-import 'package:budgetize/incomeScreen.dart';
+import 'package:budgetize/summaryScreen.dart';
+import 'package:budgetize/transactionHistoryScreen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'placeholder_widget.dart';
-import 'incomeScreen.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -16,10 +15,11 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   int _currentIndex = 0;
   final List<Widget> _children = [
-    PlaceholderWidget(Colors.white),
-    PlaceholderWidget(Colors.deepOrange),
+    SummaryScreen(),
+    TransactionHistoryScreen(),
     PlaceholderWidget(Colors.green)
   ];
+
 
   @override
   Widget build(BuildContext context) {
@@ -28,54 +28,29 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         title: Text('Budgetize'),
       ),
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  width: 80,
-                  height: 80,
-                  child: SizedBox(
-                    child: FloatingActionButton(
-                        heroTag: "removeButton",
-                        elevation: 0,
-                        onPressed: () {
-                          print("Remove button pressed.");
-                        },
-                        backgroundColor: Colors.red,
-                        child: Icon(
-                          Icons.remove,
-                          size: 45,
-                        )), // not implemented yet
-                  ),
-                ),
-                Container(
-                  width: 80,
-                  height: 80,
-                  margin: EdgeInsets.only(bottom: 10),
-                  child: SizedBox(
-                    child: FloatingActionButton(
-                        heroTag: "addButton",
-                        elevation: 0,
-                        onPressed: () {
-                          print("Add button pressed.");
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => IncomeScreen()));
-                        },
-                        backgroundColor: Colors.green,
-                        child:
-                            Icon(Icons.add, size: 45)), // not implemented yet
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed, // makes more than 3 items possible
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.black.withOpacity(.60),
+        onTap: (value) {
+          setState(() => _currentIndex = value);
+          print("Navigation bar pressed. Current index is $_currentIndex.");
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.pie_chart),
+            title: new Text('Podsumowanie'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.bar_chart),
+            title: new Text('Analiza'),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.schedule), title: Text('Transakcje')),
+        ],
       ),
-      bottomNavigationBar: NavigationBar(),
     );
   }
 }
---
+++ lib/summaryScreen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'incomeScreen.dart';
+
+class SummaryScreen extends StatefulWidget {
+  @override
+  _SummaryScreenState createState() => _SummaryScreenState();
+}
+
+class _SummaryScreenState extends State<SummaryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 80,
+                height: 80,
+                child: SizedBox(
+                  child: FloatingActionButton(
+                      heroTag: "removeButton",
+                      elevation: 0,
+                      onPressed: () {
+                        print("Remove button pressed.");
+                      },
+                      backgroundColor: Colors.red,
+                      child: Icon(
+                        Icons.remove,
+                        size: 45,
+                      )), // not implemented yet
+                ),
+              ),
+              Container(
+                width: 80,
+                height: 80,
+                margin: EdgeInsets.only(bottom: 10),
+                child: SizedBox(
+                  child: FloatingActionButton(
+                      heroTag: "addButton",
+                      elevation: 0,
+                      onPressed: () {
+                        print("Add button pressed.");
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => IncomeScreen()));
+                      },
+                      backgroundColor: Colors.green,
+                      child:
+                      Icon(Icons.add, size: 45)), // not implemented yet
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
