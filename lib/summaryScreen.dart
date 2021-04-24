1c2b7ad65163c7ea3cf21aa093406a1223e6bacb
bizkit53 <ciuba.piotr@gmail.com>
2021-04-24 09:42:13 +0200

    Weekly expenses bar chart: proper data provided, styling improved

--- lib/summaryScreen.dart
+++ lib/summaryScreen.dart
@@ -4,7 +4,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:intl/intl.dart';
 import 'transactionScreen.dart';
+
+DateFormat formatter = DateFormat('dd-MM');
+DateTime today = DateTime.now();
 
 class SummaryScreen extends StatefulWidget {
   @override
@@ -12,8 +16,66 @@ class SummaryScreen extends StatefulWidget {
 }
 
 class _SummaryScreenState extends State<SummaryScreen> {
+  List<DateTime> days = [];
+  List<String> daysFormatted = [];
+  var transactionsBox = Hive.box<Transaction>('transactions');
+  bool initialized = false;
+  List<double> daySpendingAmount = [];
+  List<String> daySpendingAmountCompactString = [];
+  List<int> showTooltipIndicator = [];
+  double amount;
+  double maxSpendingValue = 0;
+
+  void initDays () {
+    for(int i = 6; i >= 0; i--){
+      var nextDay = today.subtract(Duration(days: i));
+      var nextDayFormatted = formatter.format(nextDay);
+      days.add(nextDay);
+      daysFormatted.add(nextDayFormatted);
+      daySpendingAmount.insert(6-i, 0);
+      showTooltipIndicator.insert(6-i, 0);
+      daySpendingAmountCompactString.insert(6-i, "");
+    }
+  }
+
+  void calculateDaySpendings() {
+    for (int i = 0; i < transactionsBox.length; i++) {
+      var transaction = transactionsBox.getAt(i);
+
+      for (int j = 6; j >= 0; j--) {
+        var date = days.elementAt(j);
+
+        if (transaction.type == TransactionType.expenditure &&
+            transaction.date.year == date.year &&
+            transaction.date.month == date.month &&
+            transaction.date.day == date.day ) {
+
+          daySpendingAmount[j] += transaction.amount;
+        }
+      }
+    }
+
+    for (int j = 6; j >= 0; j--) {
+      if (daySpendingAmount[j] > maxSpendingValue)
+        maxSpendingValue = daySpendingAmount[j] * 1.4;
+
+      if (daySpendingAmount[j] != 0)
+        showTooltipIndicator[j] = 0;
+      else
+        showTooltipIndicator[j] = 1;
+
+      daySpendingAmountCompactString[j] = NumberFormat.compact().format(daySpendingAmount[j]);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    if(initialized == false) {
+      initDays();
+      calculateDaySpendings();
+      initialized = true;
+    }
+
     return Container(
       color: Color.fromRGBO(223, 223, 223, 100),
       child: SafeArea(
@@ -64,62 +126,106 @@ class _SummaryScreenState extends State<SummaryScreen> {
                             color: Theme.of(context).primaryColor,
                             borderRadius: BorderRadius.vertical(top: Radius.circular(25)),
                           ),
-                          child: Text("Last week expenses", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),),
+                          child: Text("Weekly expenses", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),),
                         ),
                         Expanded(
                           child: Container(
-                            transform: Matrix4.translationValues(-20, 10, 0),
                             child: BarChart(
                               BarChartData(
                                 alignment: BarChartAlignment.spaceAround,
+                                maxY: maxSpendingValue,
+                                barTouchData: BarTouchData(
+                                  enabled: false,
+                                  touchTooltipData: BarTouchTooltipData(
+                                    tooltipBgColor: Colors.indigo,
+                                    tooltipPadding: const EdgeInsets.only(bottom: -3),
+                                    tooltipMargin: 6,
+                                    getTooltipItem: (
+                                        BarChartGroupData group,
+                                        int groupIndex,
+                                        BarChartRodData rod,
+                                        int rodIndex,
+                                        ) {
+                                      return BarTooltipItem(
+                                        daySpendingAmountCompactString[groupIndex],
+                                        TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 14,
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                ),
+                                titlesData: FlTitlesData(
+                                  show: true,
+                                  bottomTitles: SideTitles(
+                                    showTitles: true,
+                                    getTextStyles: (value) => const TextStyle(
+                                        color: Color(0xff7589a2), fontWeight: FontWeight.bold, fontSize: 12),
+                                    getTitles: (double value) {
+                                      return daysFormatted.elementAt(value.toInt());
+                                    },
+                                  ),
+                                  leftTitles: SideTitles(
+                                    showTitles: false,
+                                  ),
+                                ),
                                 borderData: FlBorderData(
-                                  border: Border(bottom: BorderSide(width: 1), left: BorderSide(width: 1)),
+                                  show: false,
                                 ),
                                 barGroups: [
                                   BarChartGroupData(
+                                    x: 0,
+                                    barRods: [
+                                      BarChartRodData(y: daySpendingAmount[0], colors: [Colors.lightBlueAccent, Colors.greenAccent])
+                                    ],
+                                    showingTooltipIndicators: [showTooltipIndicator[0]],
+                                  ),
+                                  BarChartGroupData(
                                     x: 1,
                                     barRods: [
-                                      BarChartRodData(y: 1),
+                                      BarChartRodData(y: daySpendingAmount[1], colors: [Colors.lightBlueAccent, Colors.greenAccent])
                                     ],
+                                    showingTooltipIndicators: [showTooltipIndicator[1]],
                                   ),
                                   BarChartGroupData(
                                     x: 2,
                                     barRods: [
-                                      BarChartRodData(y: 2),
+                                      BarChartRodData(y: daySpendingAmount[2], colors: [Colors.lightBlueAccent, Colors.greenAccent])
                                     ],
+                                    showingTooltipIndicators: [showTooltipIndicator[2]],
                                   ),
                                   BarChartGroupData(
                                     x: 3,
                                     barRods: [
-                                      BarChartRodData(y: 3),
+                                      BarChartRodData(y: daySpendingAmount[3], colors: [Colors.lightBlueAccent, Colors.greenAccent])
                                     ],
+                                    showingTooltipIndicators: [showTooltipIndicator[3]],
                                   ),
                                   BarChartGroupData(
                                     x: 4,
                                     barRods: [
-                                      BarChartRodData(y: 4),
+                                      BarChartRodData(y: daySpendingAmount[4], colors: [Colors.lightBlueAccent, Colors.greenAccent])
                                     ],
+                                    showingTooltipIndicators: [showTooltipIndicator[4]],
                                   ),
                                   BarChartGroupData(
                                     x: 5,
                                     barRods: [
-                                      BarChartRodData(y: 5),
+                                      BarChartRodData(y: daySpendingAmount[5], colors: [Colors.lightBlueAccent, Colors.greenAccent])
                                     ],
+                                    showingTooltipIndicators: [showTooltipIndicator[5]],
                                   ),
                                   BarChartGroupData(
                                     x: 6,
                                     barRods: [
-                                      BarChartRodData(y: 6),
+                                      BarChartRodData(y: daySpendingAmount[6], colors: [Colors.lightBlueAccent, Colors.greenAccent])
                                     ],
-                                  ),
-                                  BarChartGroupData(
-                                    x: 7,
-                                    barRods: [
-                                      BarChartRodData(y: 7),
-                                    ],
+                                    showingTooltipIndicators: [showTooltipIndicator[6]],
                                   ),
                                 ],
-                              )
+                              ),
                             ),
                           ),
                         ),
@@ -161,12 +267,16 @@ class _SummaryScreenState extends State<SummaryScreen> {
                     height: 80,
                     child: SizedBox(
                       child: FloatingActionButton(
-                          heroTag: "removeButton",
+                          heroTag: "addExpenditureButton",
                           elevation: 0,
                           onPressed: () {
-                            print("Remove button pressed.");
+                            print("Add expenditure button pressed.");
                             Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => TransactionScreen(transactionType: TransactionType.expenditure,)));
+                                builder: (context) => TransactionScreen(transactionType: TransactionType.expenditure,))).then((value) {
+                                  setState(() {
+                                    initialized = false;
+                                  });
+                                });
                           },
                           backgroundColor: Colors.red,
                           child: Icon(
@@ -181,16 +291,19 @@ class _SummaryScreenState extends State<SummaryScreen> {
                     margin: EdgeInsets.only(bottom: 10),
                     child: SizedBox(
                       child: FloatingActionButton(
-                          heroTag: "addButton",
+                          heroTag: "addIncomeButton",
                           elevation: 0,
                           onPressed: () {
-                            print("Add button pressed.");
+                            print("Add income button pressed.");
                             Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => TransactionScreen(transactionType: TransactionType.income,)));
+                                builder: (context) => TransactionScreen(transactionType: TransactionType.income,))).then((value) {
+                                  setState(() {
+                                    initialized = false;
+                                  });
+                                });
                           },
                           backgroundColor: Colors.green,
-                          child:
-                          Icon(Icons.add, size: 45)),
+                          child: Icon(Icons.add, size: 45)),
                     ),
                   ),
                 ],
